385cdbdbdf9a6f7d705f36231d95bb20001da7c6
azcvcza <283940802@qq.com>
2019-04-04 21:36:00 +0800

    feat: use full toolchain to build react project

---
+++ react-typescript/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const root = document.querySelector(".container");
+
+ReactDOM.render(<h1>fuck you bitch</h1>,root);
---
+++ react-typescript/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+    entry:"./src/index.tsx",
+    output:{
+        path:path.resolve(__dirname,"./build"),
+        filename:"bundle.js"
+    },
+
+    module:{
+        rules:[
+            {
+                test:/\.tsx?$/,
+                loader:"awesome-typescript-loader"
+            }
+        ]
+    },
+    plugins:[]
+}
