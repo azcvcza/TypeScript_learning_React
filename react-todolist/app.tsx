59a887d2e7c2756d436a6e6da204b571cc3c179b
azcvcza <283940802@qq.com>
2019-04-07 15:44:04 +0800

    complete a todo demo

--- react-todolist/app.tsx
+++ react-todolist/app.tsx
@@ -1,5 +1,58 @@
-import * as React from 'react';
-function App():JSX.Element{
-    return <h1>Hi</h1>
+import React, { Fragment, useState } from 'react';
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+interface ITodo {
+    text: string;
+    complete: boolean;
+}
+function App(): JSX.Element {
+    const [value, setValue] = useState<string>('');
+    const [todos, setTodos] = useState<ITodo[]>([]);
+    const addTodo = (text: string): void => {
+        const newTodos: ITodo[] = [...todos, { text, complete: false }];
+        setTodos(newTodos);
+    };
+    const completeTodo = (index: number): void => {
+        const newTodos: ITodo[] = todos
+        newTodos[index].complete = !newTodos[index].complete
+        setTodos(newTodos)
+    };
+    const removeTodo = (index: number): void => {
+        const newTodos: ITodo[] = todos
+        newTodos.splice(index, 1)
+        setTodos(newTodos)
+    }
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        setValue("")
+    }
+    console.log(todos)
+    return (<Fragment>
+        <h1>Todo List</h1>
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                value={value}
+                onChange={e => setValue(e.target.value)}
+                required
+            />
+            <button type='submit'>Add Todo</button>
+        </form>
+        <section>
+            {todos.map((todo: ITodo, index: number) => (
+                <div key={index} style={{ display: 'flex' }}>
+                    <div
+                        style={{ textDecoration: todo.complete ? 'line-through' : '' }}
+                    >
+                        {todo.text}
+                    </div>
+                    <button type='button' onClick={() => completeTodo(index)}>
+                        {todo.complete ? 'Incomplete' : 'Complete'}
+                    </button>
+                    <button onClick={() => removeTodo(index)}>x</button>
+                </div>
+            ))}
+        </section>
+    </Fragment>)
 }
 export default App
